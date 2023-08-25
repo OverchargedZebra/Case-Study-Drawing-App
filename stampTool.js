09c372fad094f0c074bb3d5d074d518a30adf0d4
OverchargedZebra <waleedbinshabbir@gmail.com>
2023-08-25 10:05:56 +0500

    made some changes to stamp - incomplete

--- stampTool.js
+++ stampTool.js
@@ -2,7 +2,22 @@ function stampTool() {
 	this.icon = "assets/stampTool.png";
 	this.name = "stampTool";
 
-	var stampImage = loadImage("assets/stampTool.png");
+	var stamps = [
+		"assets/stampTool.png",
+		"assets/stampImage2.png",
+		"assets/stampImage3.png",
+		"assets/stampImage4.png",
+		"assets/stampImage5.png",
+		"assets/stampImage6.png",
+		"assets/stampImage7.png",
+		"assets/stampImage8.png",
+	];
+
+	for (var i = 0; i < stamps.length; i++) {
+		stamps[i] = loadImage(stamps[i]);
+	}
+
+	var stampImage = stamps[0];
 
 	var number = 1;
 
@@ -29,5 +44,31 @@ function stampTool() {
 				);
 			}
 		}
+	};
+
+	var originalSwatchStyle = [];
+	var colourSwatches = document.getElementsByClassName("colourSwatches");
+
+	this.unselectTool = function () {
+		loadPixels();
+
+		for (let i = 0; i < colourSwatches.length; i++) {
+			colourSwatches[i].style = originalSwatchStyle[i];
+		}
+
+		colourP.loadColours();
+
+		document.getElementById("colour-picker").style = "display: colourP;";
+
+		select(".options").html("");
+	};
+
+	this.populateOptions = function () {
+		for (let i = 0; i < colourSwatches.length; i++) {
+			originalSwatchStyle.push(colourSwatches[i].style);
+			colourSwatches[i].style = "display: none;";
+		}
+
+		document.getElementById("colour-picker").style = "display: none;";
 	};
 }
